a784d0e4a64a5e98ec6441dbd9da999f618755f7
probison <parker.robison@hotmail.com>
2020-09-24 23:34:15 -0600

    added idb storage to cart, products, and categories for offline functionality

--- client/src/components/ProductList/index.js
+++ client/src/components/ProductList/index.js
@@ -6,6 +6,7 @@ import { UPDATE_PRODUCTS } from '../../utils/actions';
 import ProductItem from "../ProductItem";
 import { QUERY_PRODUCTS } from "../../utils/queries";
 import spinner from "../../assets/spinner.gif"
+import { idbPromise } from "../../utils/helpers";
 
 function ProductList() {
   // const { loading, data } = useQuery(QUERY_PRODUCTS);
@@ -27,13 +28,30 @@ function ProductList() {
   const { loading, data } = useQuery(QUERY_PRODUCTS);
 
   useEffect(() => {
+    // if there is data to be stored
     if (data) {
+      // lets store it in the global state object
       dispatch({
         type: UPDATE_PRODUCTS,
         products: data.products
       });
+
+      //but let's also take each product and save it to IndexedDB using the helper function
+      data.products.forEach((product) => {
+        idbPromise('products', 'put', product);
+      });
+      // add else if to check if `loading` is undefined in `useQuery()` Hook
+    } else if (!loading) {
+      //since we're offline, get all of the data from the `products` store
+      idbPromise('products', 'get').then((products) => {
+        // use retrieved data to set global state for offline browsing
+        dispatch({
+          type: UPDATE_PRODUCTS,
+          products: products
+        });
+      });
     }
-  }, [data, dispatch]);
+  }, [data, loading, dispatch]);
 
   function filterProducts() {
     if (!currentCategory) {
