24c1be32be26601ba4657e1f74135150fbaea597
probison <parker.robison@hotmail.com>
2020-09-25 23:29:51 -0600

    integrated stripe into the app

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -4,10 +4,16 @@ import Auth from '../../utils/auth';
 import './style.css';
 import { useStoreContext } from '../../utils/GlobalState';
 import { TOGGLE_CART, ADD_MULTIPLE_TO_CART } from '../../utils/actions';
-import { ibdPromise, idbPromise } from "../../utils/helpers";
+import { idbPromise } from "../../utils/helpers";
+import { QUERY_CHECKOUT } from '../../utils/queries';
+import { loadStripe } from '@stripe/stripe-js';
+import { useLazyHook, useLazyQuery } from '@apollo/react-hooks';
+
+const stripePromise = loadStripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
 
 const Cart = () => {
     const [state, dispatch] = useStoreContext();
+    const [getCheckout, { data }] =useLazyQuery(QUERY_CHECKOUT);
 
     useEffect(() => {
         async function getCart() {
@@ -32,6 +38,26 @@ const Cart = () => {
         return sum.toFixed(2);
     }
 
+    function submitCheckout() {
+        const productIds = [];
+        state.cart.forEach((item) => {
+            for (let i = 0; i < item.purchaseQuantity; i++) {
+                productIds.push(item._id);
+            }
+        });
+        getCheckout({
+            variables: { products: productIds }
+        });
+    }
+
+    useEffect(() => {
+        if (data) {
+            stripePromise.then((res) => {
+                res.redirectToCheckout({ sessionId: data.checkout.session });
+            });
+        }
+    }, [data]);
+
     if (!state.cartOpen) {
         return (
             <div className="cart-closed" onClick={toggleCart}>
@@ -55,7 +81,7 @@ const Cart = () => {
                 <strong>Total: ${calculateTotal()}</strong>
                 {
                 Auth.loggedIn() ?
-                    <button>
+                    <button onClick= {submitCheckout}>
                     Checkout
                     </button>
                     :
